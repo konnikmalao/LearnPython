6467e8d9f2288867dcfb8cb0f085cc18caf223d2
konnikmalao <40271418+konnikmalao@users.noreply.github.com>
2018-06-14 22:46:54 +0700

    Update Exercise1.py

--- Exercise1.py
+++ Exercise1.py
@@ -1,4 +1,24 @@
 #Exercise1.py
 #This program is to calculate sum of two arbitrary numbers
 
-print('
+print('Please enter the first number: ')
+While True:
+  try:
+      x = float(input(''))
+  except ValueError:
+      print('Your data is not a number. Please enter again: ')
+  continue
+  else:
+      break
+      
+print('Please enter the second number: ')
+While True:
+  try:
+      y = float(input(''))
+  except ValueError:
+      print('Your data is not a number. Please enter again: ')
+  continue
+  else:
+      break
+      
+print('Sum of the two numbers is: ', x + y)
